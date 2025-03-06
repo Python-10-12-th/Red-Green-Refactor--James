a89b503afba455f5f0784bc5c35e040502e77392
pul1234 <pulj250@aa.edu>
2025-03-06 09:07:57 -0700

    Wrote passing code II

--- roman.py
+++ roman.py
@@ -1,6 +1,6 @@
 def convert_roman_numerals(s):	
 	count =0
 	for char in s:
-		if char == "I":	
+		if char == "I":
 		 count+=1
 	return count
--- roman_test.py
+++ roman_test.py
@@ -1,9 +1,10 @@
 import unittest
-from roman import convert_roman_numerals  # Import the function directly
+from roman import convert_roman_numerals 
 
 class TestRoman(unittest.TestCase):
     def test_I(self):
-        self.assertEqual(convert_roman_numerals("I"), 1)  # Call function correctly
-
+        self.assertEqual(convert_roman_numerals("I"), 1)  
+    def test_II(self):
+        self.assertEqual(convert_roman_numerals("II"), 2) 
 if __name__ == '__main__':
     unittest.main()
